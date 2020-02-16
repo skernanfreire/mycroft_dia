bdd9398da384d3026261dda799ce88f5f215ca74
skernanfreire <skernanfreire@gmail.com>
2020-02-16 21:29:45 +0100

    adapt intents maintenance and status w context

--- __init__.py
+++ __init__.py
@@ -17,13 +17,25 @@ class DiamondAssistant(MycroftSkill):
     def handle_machine_status_intent(self, message):
         """ This is a Padatious intent handler.
         It is triggered using a list of sample phrases."""
+        self.log.info("padatious status intent was triggered")
         machine_number = message.data.get('number')
         self.speak_dialog('machine.status',{'number': machine_number})
 
-    @intent_handler(IntentBuilder('ThankYouIntent').require('ThankYouKeyword'))
-    def handle_thank_you_intent(self, message):
+    @intent_handler(IntentBuilder('MaintenanceIntent').optionally('MachineKeyword').require('MaintenanceKeyword').require('number'))
+    def handle_maintenance_intent(self, message):
         """ This is an Adapt intent handler, it is triggered by a keyword."""
-        self.speak_dialog("welcome")
+        self.log.info("adapt maintenance intent was triggered")
+        machine_number = message.data.get('Number')
+        self.speak_dialog("machine.maintenance",{'number': machine_number})
+        self.set_context('number', machine_number)
+
+    @intent_handler(IntentBuilder('AdaptStatusIntent').optionally('MachineKeyword').require('MachineStatusKeyword').require('number'))
+    def handle_adapt_status_intent(self, message):
+        """ This is an Adapt intent handler, it is triggered by a keyword."""
+        self.log.info("adapt status intent was triggered")
+        machine_number = message.data.get('number')
+        self.speak_dialog("machine.maintenance",{'number': machine_number})
+        self.set_context('number', machine_number)
 
     def stop(self):
         pass
