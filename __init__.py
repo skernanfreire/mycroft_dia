dfc9eda2a333ad02a8c3539684bcc36fc0c7c21e
skernanfreire <skernanfreire@gmail.com>
2020-02-17 10:27:43 +0100

    enabled padatious intents, disabled adapt

--- __init__.py
+++ __init__.py
@@ -13,15 +13,26 @@ class DiamondAssistant(MycroftSkill):
         settings will be available."""
         my_setting = self.settings.get('my_setting')'''
 
-    '''@intent_file_handler('machine.status.intent')
+    @intent_file_handler('machine.status.intent')
     def handle_machine_status_intent(self, message):
         """ This is a Padatious intent handler.
         It is triggered using a list of sample phrases."""
         self.log.info("padatious status intent was triggered")
         machine_number = message.data.get('number')
-        self.speak_dialog('machine.status',{'number': machine_number})'''
+        self.speak_dialog('machine.status',{'number': machine_number})
+        self.set_context('number', machine_number)
 
-    @intent_handler(IntentBuilder('MaintenanceIntent').optionally('MachineKeyword').require('MaintenanceKeyword').require('number'))
+    @intent_file_handler('machine.maintenance.intent')
+    def handle_machine_status_intent(self, message):
+        """ This is a Padatious intent handler.
+        It is triggered using a list of sample phrases."""
+        self.log.info("padatious status intent was triggered")
+        machine_number = message.data.get('number')
+        self.speak_dialog('machine.maintenance',{'number': machine_number})
+        self.set_context('number', machine_number)
+        
+
+    '''@intent_handler(IntentBuilder('MaintenanceIntent').optionally('MachineKeyword').require('MaintenanceKeyword').require('number'))
     def handle_maintenance_intent(self, message):
         """ This is an Adapt intent handler, it is triggered by a keyword."""
         self.log.info("adapt maintenance intent was triggered")
@@ -37,7 +48,7 @@ class DiamondAssistant(MycroftSkill):
         machine_number = message.data.get('number')
         self.log.info(machine_number)
         self.speak_dialog("machine.status",{'number': machine_number})
-        self.set_context('number', machine_number)
+        self.set_context('number', machine_number)'''
       
         
 
